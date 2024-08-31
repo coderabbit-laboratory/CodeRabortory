238c85fd3adabf00db38048c316bad0909afd87c
bloomspes <bloomspes@gmail.com>
2024-09-01 01:27:12 +0900

    Create spring boot project
    Create a new project using Start Initializr(https://start.spring.io/).

---
+++ src/main/kotlin/com/github/coderabortory_spring/CoderabortorySpringApplication.kt
@@ -0,0 +1,11 @@
+package com.github.coderabortory_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoderabortorySpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoderabortorySpringApplication>(*args)
+}
